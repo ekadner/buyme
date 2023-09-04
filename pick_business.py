729f8769fbbac3170944735202d3f33513c41ca9
ekadner <ekadner@gmailcom>
2023-09-04 21:34:44 +0300

    04.09.23

--- pick_business.py
+++ pick_business.py
@@ -51,3 +51,23 @@ class PickBusinessScreen:
         current_url = self.driver.current_url
         expected_url = "https://buyme.co.il/money/20620?price=250"
         assert current_url == expected_url, f"Expected URL: {expected_url}, Actual URL: {current_url}"
+
+
+# def pick_category(self):
+#     pick_category = self.wait.until(expected_conditions.presence_of_element_located(
+#         (By.XPATH, "//a[@role='button' and @title='גיפט קארד למותגי אופנה']")))
+#     self.execute_script("arguments[0].scrollIntoView();", pick_category)
+#     pick_category.click()
+#
+# def choose_gift_card(self):
+#     gift_card = self.wait.until(expected_conditions.presence_of_element_located((By.XPATH,
+#                                                                    "//a[@aria-label='            \n\n        \n            \n        \n\n\n\n\n\n    \n        BUYME TOTAL FASHION\n    \n\n\n']")))
+#     gift_card.click()
+#
+# def fill_in_an_amount(self):
+#     amount = self.wait.until(expected_conditions.presence_of_element_located(By.XPATH, "//input[@placeholder='הכנס סכום']"))
+#     amount.send_keys("250")
+#
+# def click_submit_button(self):
+#     submit = self.wait.until(expected_conditions.presence_of_element_located((By.XPATH, "//button[@type='submit']")))
+#     submit.click()
